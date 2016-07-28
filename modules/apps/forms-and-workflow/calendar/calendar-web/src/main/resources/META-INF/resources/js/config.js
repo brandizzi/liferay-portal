b5e713934d4461022a334f7aa334fa693ee47021
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-07-28 16:29:37 -0300

    LPS-67370 / LPS-64589 Move Scheduler models outside scheduler.js

--- modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/config.js
+++ modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/config.js
@@ -115,6 +115,15 @@
 								'promise',
 								'resize-plugin'
 							]
+						},
+						'liferay-scheduler-models': {
+							path: 'scheduler_models.js',
+							requires: [
+								'aui-datatype',
+								'dd-plugin',
+								'liferay-calendar-util',
+								'liferay-store'
+							]
 						}
 					},
 					root: MODULE_PATH + '/js/'
