26de05cfefdd2c51eccf2a31298fe56d30b5ad29
André de Oliveira <andre.oliveira@liferay.com>
2017-03-31 19:50:32 -0700

    AssetEntrySearchResultImageContributor

---
+++ modules/apps/web-experience/asset/asset-web/src/main/java/com/liferay/asset/web/internal/search/AssetEntrySearchResultImageContributor.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.web.internal.search;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.portal.search.web.search.result.SearchResultImage;
+import com.liferay.portal.search.web.search.result.SearchResultImageContributor;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author André de Oliveira
+ */
+@Component(service = SearchResultImageContributor.class)
+public class AssetEntrySearchResultImageContributor
+	implements SearchResultImageContributor {
+
+	@Override
+	public void contribute(SearchResultImage searchResultImage) {
+		String className = searchResultImage.getClassName();
+
+		if (className.equals(AssetEntry.class.getName())) {
+			searchResultImage.setIcon("web-content");
+		}
+	}
+
+}
