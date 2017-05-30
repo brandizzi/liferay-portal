4f69bf7a5c144b4e1f47bb4cfed27b746e12702d
dylan-rebelak <dylan.rebelak@liferay.com>
2017-05-30 11:24:48 -0500

    Temp ES fixes

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/RemoteElasticsearchConnection.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/RemoteElasticsearchConnection.java
@@ -179,7 +179,7 @@ public class RemoteElasticsearchConnection extends BaseElasticsearchConnection {
 		settingsBuilder.put("http.enabled", false);
 		settingsBuilder.put("node.data", false);
 		settingsBuilder.put("node.ingest", false);
-		settingsBuilder.put("node.master", false);
+		settingsBuilder.put("node.master", true);
 		settingsBuilder.put(
 			"path.logs", props.get(PropsKeys.LIFERAY_HOME) + "/logs");
 	}
