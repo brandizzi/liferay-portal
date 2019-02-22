96b5e351994f91157495bc59dd802fbe129b84d1
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2019-02-22 12:35:19 -0300

    fixup! LPS-90068 contacts-service: Fix package name https://issues.liferay.com/browse/LPS-90068

--- modules/apps/contacts/contacts-service/src/main/java/com/liferay/contacts/internal/search/spi/model/query/contributor/ContactKeywordQueryContributor.java
+++ modules/apps/contacts/contacts-service/src/main/java/com/liferay/contacts/internal/search/spi/model/query/contributor/ContactKeywordQueryContributor.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.contact.internal.search.spi.model.query.contributor;
+package com.liferay.contacts.internal.search.spi.model.query.contributor;
 
 import com.liferay.portal.kernel.search.BooleanQuery;
 import com.liferay.portal.kernel.search.SearchContext;
