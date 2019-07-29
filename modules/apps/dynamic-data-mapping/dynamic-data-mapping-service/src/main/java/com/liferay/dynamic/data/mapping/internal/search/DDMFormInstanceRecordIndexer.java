585fa5f2843318f8f9fa7d75a4aed65987b5ec54
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2019-07-29 15:52:00 -0300

    fixup! LPS-85384 dynamic-data-mapping-service: Deprecate DDMFormInstance and DDMFormInstanceRecord legacy indexer.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/search/DDMFormInstanceRecordIndexer.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/search/DDMFormInstanceRecordIndexer.java
@@ -63,7 +63,7 @@ import javax.portlet.PortletResponse;
 
 /**
  * @author Leonardo Barros
- * @deprecated As of Judson (7.1.x), since 7.1.0
+ * @deprecated As of Judson (7.1.x)
  */
 @Deprecated
 public class DDMFormInstanceRecordIndexer
