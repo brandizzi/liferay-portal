edaf186142bd5b86ff71f3ddc1dc780e12a8dd26
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2019-12-04 17:03:42 -0300

    LPS-100272 portal-search-elasticsearch7-impl: Test for index contributors in Elasticsearch 6
    https://issues.liferay.com/browse/LPS-100272

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/index/CompanyIndexFactoryTest.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/index/CompanyIndexFactoryTest.java
@@ -21,10 +21,12 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.search.elasticsearch6.internal.connection.ElasticsearchFixture;
 import com.liferay.portal.search.elasticsearch6.internal.connection.IndexName;
 import com.liferay.portal.search.elasticsearch6.internal.document.SingleFieldFixture;
+import com.liferay.portal.search.elasticsearch6.internal.query.QueryBuilderFactories;
 import com.liferay.portal.search.elasticsearch6.internal.settings.BaseIndexSettingsContributor;
 import com.liferay.portal.search.elasticsearch6.internal.util.ResourceUtil;
 import com.liferay.portal.search.elasticsearch6.settings.IndexSettingsHelper;
 import com.liferay.portal.search.elasticsearch6.settings.TypeMappingsHelper;
+import com.liferay.portal.search.spi.model.index.contributor.IndexContributor;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -66,6 +68,8 @@ public class CompanyIndexFactoryTest {
 			_elasticsearchFixture.getClient(),
 			new IndexName(_companyIndexFactoryFixture.getIndexName()),
 			LiferayTypeMappingsConstants.LIFERAY_DOCUMENT_TYPE);
+
+		_singleFieldFixture.setQueryBuilderFactory(QueryBuilderFactories.MATCH);
 	}
 
 	@After
@@ -175,6 +179,27 @@ public class CompanyIndexFactoryTest {
 	}
 
 	@Test
+	public void testIndexContributors() throws Exception {
+		addIndexContributor(
+			indexName -> indexOneDocument(
+				"contributedField", "contributed value"));
+
+		createIndices();
+
+		_singleFieldFixture.assertSearch("contributed", "contributed value");
+	}
+
+	@Test
+	public void testIndexContributorsThrowsException() throws Exception {
+		addIndexContributor(
+			indexName -> {
+				throw new RuntimeException();
+			});
+
+		createIndices();
+	}
+
+	@Test
 	public void testIndexSettingsContributor() throws Exception {
 		_companyIndexFactory.addIndexSettingsContributor(
 			new BaseIndexSettingsContributor(1) {
@@ -277,6 +302,10 @@ public class CompanyIndexFactoryTest {
 
 	@Rule
 	public TestName testName = new TestName();
+
+	protected void addIndexContributor(IndexContributor indexContributor) {
+		_companyIndexFactory.addIndexContributor(indexContributor);
+	}
 
 	protected void addIndexSettingsContributor(String mappings) {
 		_companyIndexFactory.addIndexSettingsContributor(
@@ -384,9 +413,13 @@ public class CompanyIndexFactoryTest {
 	}
 
 	protected void indexOneDocument(String field) {
+		indexOneDocument(field, RandomTestUtil.randomString());
+	}
+
+	protected void indexOneDocument(String field, String value) {
 		_singleFieldFixture.setField(field);
 
-		_singleFieldFixture.indexDocument(RandomTestUtil.randomString());
+		_singleFieldFixture.indexDocument(value);
 	}
 
 	protected String loadAdditionalAnalyzers() throws Exception {
