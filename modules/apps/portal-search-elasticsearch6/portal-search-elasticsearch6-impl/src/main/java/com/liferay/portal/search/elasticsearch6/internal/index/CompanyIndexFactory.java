9155f5a5820ee7a1260d0b830eb259fef60395ff
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2019-12-04 14:15:08 -0300

    LPS-100272 portal-search-elasticsearch6-impl: Apply contributors after index creation on Elasticsearch 6
    https://issues.liferay.com/browse/LPS-100272

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/index/CompanyIndexFactory.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/index/CompanyIndexFactory.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.search.elasticsearch6.internal.index;
 
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.log.Log;
@@ -27,10 +28,13 @@ import com.liferay.portal.search.elasticsearch6.internal.util.ResourceUtil;
 import com.liferay.portal.search.elasticsearch6.settings.IndexSettingsContributor;
 import com.liferay.portal.search.elasticsearch6.settings.IndexSettingsHelper;
 import com.liferay.portal.search.index.IndexNameBuilder;
+import com.liferay.portal.search.spi.model.index.contributor.IndexContributor;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.elasticsearch.action.ActionResponse;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
@@ -115,6 +119,15 @@ public class CompanyIndexFactory implements IndexFactory {
 		policy = ReferencePolicy.DYNAMIC,
 		policyOption = ReferencePolicyOption.GREEDY
 	)
+	protected void addIndexContributor(IndexContributor indexContributor) {
+		_indexContributors.add(indexContributor);
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
 	protected void addIndexSettingsContributor(
 		IndexSettingsContributor indexSettingsContributor) {
 
@@ -155,6 +168,29 @@ public class CompanyIndexFactory implements IndexFactory {
 		LogUtil.logActionResponse(_log, createIndexResponse);
 
 		updateLiferayDocumentType(indexName, liferayDocumentTypeFactory);
+
+		executeIndexContributorsAfterCreate(indexName);
+	}
+
+	protected void executeIndexContributorAfterCreate(
+		IndexContributor indexContributor, String indexName) {
+
+		try {
+			indexContributor.onAfterCreate(indexName);
+		}
+		catch (Throwable t) {
+			_log.error(
+				StringBundler.concat(
+					"Unable to apply contributor ", indexContributor,
+					"to index ", indexName),
+				t);
+		}
+	}
+
+	protected void executeIndexContributorsAfterCreate(String indexName) {
+		for (IndexContributor indexContributor : _indexContributors) {
+			executeIndexContributorAfterCreate(indexContributor, indexName);
+		}
 	}
 
 	protected String getIndexName(long companyId) {
@@ -247,6 +283,10 @@ public class CompanyIndexFactory implements IndexFactory {
 		}
 	}
 
+	protected void removeIndexContributor(IndexContributor indexContributor) {
+		_indexContributors.remove(indexContributor);
+	}
+
 	protected void removeIndexSettingsContributor(
 		IndexSettingsContributor indexSettingsContributor) {
 
@@ -324,6 +364,8 @@ public class CompanyIndexFactory implements IndexFactory {
 
 	private volatile String _additionalIndexConfigurations;
 	private String _additionalTypeMappings;
+	private final List<IndexContributor> _indexContributors =
+		new CopyOnWriteArrayList<>();
 	private String _indexNumberOfReplicas;
 	private String _indexNumberOfShards;
 	private final Set<IndexSettingsContributor> _indexSettingsContributors =
