cdf5adbbdcb14841d9195520029c4831892da9e2
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2019-12-17 13:00:59 -0300

    LPS-100272 portal-search-tuning-synonyms-web: Instead of creating indices with index definition, we create indices when a new company is created, and also when the bundle starts up and finds companies without synonym set indices.
    https://issues.liferay.com/browse/LPS-100272

---
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/index/contributor/SynonymSetIndexCreationIndexContributor.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.search.tuning.synonyms.web.internal.index.contributor;
+
+import com.liferay.portal.search.spi.model.index.contributor.IndexContributor;
+import com.liferay.portal.search.tuning.synonyms.web.internal.index.SynonymSetIndexCreator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adam Brandizzi
+ */
+@Component(immediate = true, service = IndexContributor.class)
+public class SynonymSetIndexCreationIndexContributor
+	implements IndexContributor {
+
+	@Override
+	public void onAfterCreate(String companyIndexName) {
+		if (_synonymSetIndexCreator.isExists(companyIndexName)) {
+			return;
+		}
+
+		_synonymSetIndexCreator.create(companyIndexName);
+	}
+
+	@Reference
+	private SynonymSetIndexCreator _synonymSetIndexCreator;
+
+}
