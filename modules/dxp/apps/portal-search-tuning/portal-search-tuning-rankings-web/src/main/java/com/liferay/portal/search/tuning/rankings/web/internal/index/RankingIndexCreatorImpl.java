b22b3c87c1179a54389a3e404a97bddc1081fe47
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2020-02-11 16:08:05 -0300

    fixup! LPS-101291 portal-search-tuning-rankings-web: Move RR index creation and existence checking to a seaparate component to be reused.

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/RankingIndexCreatorImpl.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/RankingIndexCreatorImpl.java
@@ -24,6 +24,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
+ * @author Wade Cao
  * @author Adam Brandizzi
  */
 @Component(service = RankingIndexCreator.class)
