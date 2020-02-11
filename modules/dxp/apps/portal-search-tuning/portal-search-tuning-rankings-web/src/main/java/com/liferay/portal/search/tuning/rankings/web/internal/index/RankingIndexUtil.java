4ff6ef6b4d9df11e8075a0c6c70ed3fc1bbe2cbc
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2020-02-11 10:01:05 -0300

    LPS-101291 portal-search-tuning-rankings-web: Move the process of creating indices for existing documents to a new component, where we can inject all we need
    https://issues.liferay.com/browse/LPS-101291

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/RankingIndexUtil.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/RankingIndexUtil.java
@@ -14,22 +14,6 @@
 
 package com.liferay.portal.search.tuning.rankings.web.internal.index;
 
-import com.liferay.portal.search.document.Document;
-import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
-import com.liferay.portal.search.engine.adapter.document.BulkDocumentRequest;
-import com.liferay.portal.search.engine.adapter.document.BulkDocumentResponse;
-import com.liferay.portal.search.engine.adapter.document.IndexDocumentRequest;
-import com.liferay.portal.search.engine.adapter.index.DeleteIndexRequest;
-import com.liferay.portal.search.engine.adapter.index.DeleteIndexResponse;
-import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
-import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
-import com.liferay.portal.search.hits.SearchHit;
-import com.liferay.portal.search.hits.SearchHits;
-import com.liferay.portal.search.query.Queries;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Activate;
@@ -50,79 +34,9 @@ public class RankingIndexUtil {
 		return _rankingIndexUtil.getRankingIndexName1(companyIndexName);
 	}
 
-	public static void renameRankingIndexName() {
-		if (_rankingIndexUtil.isIndicesExists(
-				RankingIndexDefinition.INDEX_NAME)) {
-
-			List<Document> documents = _rankingIndexUtil.getDocuments(
-				RankingIndexDefinition.INDEX_NAME);
-
-			if (documents.isEmpty()) {
-				_rankingIndexUtil.deleteIndex(
-					RankingIndexDefinition.INDEX_NAME);
-
-				return;
-			}
-
-			Boolean succeeded = groupDocumentByIndex(
-				documents
-			).entrySet(
-			).stream(
-			).map(
-				entry -> _rankingIndexUtil.addDocuments(
-					entry.getKey(), entry.getValue())
-			).reduce(
-				true, Boolean::logicalAnd
-			);
-
-			if (succeeded) {
-				_rankingIndexUtil.deleteIndex(
-					RankingIndexDefinition.INDEX_NAME);
-			}
-		}
-	}
-
-	protected static Map<String, List<Document>> groupDocumentByIndex(
-		List<Document> documents) {
-
-		return documents.stream(
-		).collect(
-			Collectors.groupingBy(document -> document.getString("index"))
-		);
-	}
-
 	@Activate
 	protected void activate() {
 		_rankingIndexUtil = this;
-	}
-
-	protected boolean addDocuments(String indexName, List<Document> documents) {
-		boolean successed = true;
-
-		String rankingIndexName = _rankingIndexUtil.getRankingIndexName1(
-			indexName);
-
-		createRankingIndex(rankingIndexName);
-
-		BulkDocumentRequest bulkDocumentRequest = new BulkDocumentRequest();
-
-		documents.forEach(
-			document -> {
-				IndexDocumentRequest indexDocumentRequest =
-					new IndexDocumentRequest(rankingIndexName, document);
-
-				bulkDocumentRequest.addBulkableDocumentRequest(
-					indexDocumentRequest);
-			});
-
-		BulkDocumentResponse bulkDocumentResponse =
-			_searchEngineAdapter.execute(bulkDocumentRequest);
-
-		if (bulkDocumentResponse.hasErrors()) {
-			successed = false;
-		}
-
-		return successed;
 	}
 
 	protected boolean createIndex(String indexName) {
@@ -139,39 +53,6 @@ public class RankingIndexUtil {
 		createIndex(rankingIndexName);
 	}
 
-	protected boolean deleteIndex(String... indexNames) {
-		DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest(
-			indexNames);
-
-		DeleteIndexResponse deleteIndexResponse = _searchEngineAdapter.execute(
-			deleteIndexRequest);
-
-		return deleteIndexResponse.isAcknowledged();
-	}
-
-	protected List<Document> getDocuments(String indexName) {
-		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
-
-		searchSearchRequest.setIndexNames(indexName);
-		searchSearchRequest.setQuery(_queries.matchAll());
-		searchSearchRequest.setFetchSource(true);
-
-		SearchSearchResponse searchSearchResponse =
-			_searchEngineAdapter.execute(searchSearchRequest);
-
-		SearchHits searchHits = searchSearchResponse.getSearchHits();
-
-		List<SearchHit> searchHitsList = searchHits.getSearchHits();
-
-		Stream<SearchHit> documentStream = searchHitsList.stream();
-
-		return documentStream.map(
-			SearchHit::getDocument
-		).collect(
-			Collectors.toList()
-		);
-	}
-
 	protected String getRankingIndexName1(String companyIndexName) {
 		return companyIndexName + "-" + RankingIndexDefinition.INDEX_NAME;
 	}
@@ -186,28 +67,12 @@ public class RankingIndexUtil {
 		);
 	}
 
-	@Reference(unbind = "-")
-	protected void setQueries(Queries queries) {
-		_queries = queries;
-	}
-
-	@Reference(unbind = "-")
-	protected void setSearchEngineAdapter(
-		SearchEngineAdapter searchEngineAdapter) {
-
-		_searchEngineAdapter = searchEngineAdapter;
-	}
-
 	private static RankingIndexUtil _rankingIndexUtil;
-
-	private Queries _queries;
 
 	@Reference
 	private RankingIndexCreator _rankingIndexCreator;
 
 	@Reference
 	private RankingIndexReader _rankingIndexReader;
-
-	private SearchEngineAdapter _searchEngineAdapter;
 
 }
