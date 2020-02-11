4a7d73cec4d1531c5da850774da1a6bb00de029b
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2020-02-11 16:11:32 -0300

    fixup! LPS-101291 portal-search-tuning-rankings-web: Move the process of creating indices for existing documents to a new component, where we can inject all we need

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/importer/SingleIndexToMultipleIndexImporterImpl.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/importer/SingleIndexToMultipleIndexImporterImpl.java
@@ -41,6 +41,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
+ * @author Wade Cao
  * @author Adam Brandizzi
  */
 @Component(service = SingleIndexToMultipleIndexImporter.class)
