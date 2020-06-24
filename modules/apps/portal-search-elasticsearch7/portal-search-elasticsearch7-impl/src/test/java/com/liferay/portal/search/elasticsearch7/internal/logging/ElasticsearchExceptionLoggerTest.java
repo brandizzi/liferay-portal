caa8f6d2b6b5e7bbea0ed28c53afe092febb5179
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2020-06-24 19:58:25 -0300

    LPS-115942 portal-search-elasticsearch7-impl: Test: test the class which decides whether to throw or log an exception in various situations.
    https://issues.liferay.com/browse/LPS-115942

---
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/logging/ElasticsearchExceptionLoggerTest.java
@@ -0,0 +1,215 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch7.internal.logging;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.search.test.util.logging.ExpectedLogTestRule;
+
+import java.util.logging.Level;
+
+import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.rest.RestStatus;
+
+import org.hamcrest.CoreMatchers;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @author Adam Brandizzi
+ */
+public class ElasticsearchExceptionLoggerTest {
+
+	@Before
+	public void setUp() {
+		expectedLogTestRule.configure(
+			ElasticsearchExceptionLoggerTest.class, Level.WARNING);
+	}
+
+	@Test
+	public void testDeleteDocumentFailureIndexNotFoundElasticsearchStatusExceptionLogExceptionsOnlyFalse()
+		throws Throwable {
+
+		expectedLogTestRule.expectMessage(
+			CoreMatchers.not(
+				CoreMatchers.containsString(
+					ElasticsearchExceptionLogger.
+						INDEX_NOT_FOUND_EXCEPTION_MESSAGE)));
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, false);
+
+		elasticsearchExceptionLogger.logOrThrowOnDeleteDocumentFailure(
+			new ElasticsearchStatusException(
+				ElasticsearchExceptionLogger.INDEX_NOT_FOUND_EXCEPTION_MESSAGE,
+				RandomTestUtil.randomEnum(RestStatus.class)));
+	}
+
+	@Test
+	public void testDeleteDocumentFailureIndexNotFoundElasticsearchStatusExceptionLogExceptionsOnlyFalseInfoLevel()
+		throws Throwable {
+
+		expectedLogTestRule.configure(
+			ElasticsearchExceptionLoggerTest.class, Level.INFO);
+		expectedLogTestRule.expectMessage(
+			ElasticsearchExceptionLogger.INDEX_NOT_FOUND_EXCEPTION_MESSAGE);
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, false);
+
+		elasticsearchExceptionLogger.logOrThrowOnDeleteDocumentFailure(
+			new ElasticsearchStatusException(
+				ElasticsearchExceptionLogger.INDEX_NOT_FOUND_EXCEPTION_MESSAGE,
+				RandomTestUtil.randomEnum(RestStatus.class)));
+	}
+
+	@Test
+	public void testDeleteDocumentFailureIndexNotFoundElasticsearchStatusExceptionLogExceptionsOnlyTrue()
+		throws Throwable {
+
+		expectedLogTestRule.configure(
+			ElasticsearchExceptionLoggerTest.class, Level.INFO);
+		expectedLogTestRule.expectMessage(
+			ElasticsearchExceptionLogger.INDEX_NOT_FOUND_EXCEPTION_MESSAGE);
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, true);
+
+		elasticsearchExceptionLogger.logOrThrowOnDeleteDocumentFailure(
+			new ElasticsearchStatusException(
+				ElasticsearchExceptionLogger.INDEX_NOT_FOUND_EXCEPTION_MESSAGE,
+				RandomTestUtil.randomEnum(RestStatus.class)));
+	}
+
+	@Test
+	public void testDeleteDocumentFailureIndexNotFoundElasticsearchStatusExceptionLogExceptionsOnlyTrueInfoLevel()
+		throws Throwable {
+
+		expectedLogTestRule.configure(
+			ElasticsearchExceptionLoggerTest.class, Level.INFO);
+		expectedLogTestRule.expectMessage(
+			ElasticsearchExceptionLogger.INDEX_NOT_FOUND_EXCEPTION_MESSAGE);
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, true);
+
+		elasticsearchExceptionLogger.logOrThrowOnDeleteDocumentFailure(
+			new ElasticsearchStatusException(
+				ElasticsearchExceptionLogger.INDEX_NOT_FOUND_EXCEPTION_MESSAGE,
+				RandomTestUtil.randomEnum(RestStatus.class)));
+	}
+
+	@Test
+	public void testDeleteDocumentFailureIndexNotFoundLogExceptionsOnlyFalse()
+		throws Throwable {
+
+		expectedLogTestRule.configure(
+			ElasticsearchExceptionLoggerTest.class, Level.INFO);
+		expectedLogTestRule.expectMessage(
+			ElasticsearchExceptionLogger.INDEX_NOT_FOUND_EXCEPTION_MESSAGE);
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, false);
+
+		elasticsearchExceptionLogger.logOrThrowOnDeleteDocumentFailure(
+			new SearchException(
+				ElasticsearchExceptionLogger.
+					INDEX_NOT_FOUND_EXCEPTION_MESSAGE));
+	}
+
+	@Test
+	public void testDeleteDocumentFailureIndexNotFoundLogExceptionsOnlyTrue()
+		throws Throwable {
+
+		expectedLogTestRule.configure(
+			ElasticsearchExceptionLoggerTest.class, Level.INFO);
+		expectedLogTestRule.expectMessage(
+			ElasticsearchExceptionLogger.INDEX_NOT_FOUND_EXCEPTION_MESSAGE);
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, true);
+
+		elasticsearchExceptionLogger.logOrThrowOnDeleteDocumentFailure(
+			new SearchException(
+				ElasticsearchExceptionLogger.
+					INDEX_NOT_FOUND_EXCEPTION_MESSAGE));
+	}
+
+	@Test
+	public void testDeleteDocumentFailureLogExceptionsOnlyFalse()
+		throws Throwable {
+
+		expectedException.expect(SearchException.class);
+		expectedException.expectMessage(
+			"deletion failed and results in exception");
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, false);
+
+		elasticsearchExceptionLogger.logOrThrowOnDeleteDocumentFailure(
+			new SearchException("deletion failed and results in exception"));
+	}
+
+	@Test
+	public void testDeleteDocumentFailureLogExceptionsOnlyTrue()
+		throws Throwable {
+
+		expectedLogTestRule.expectMessage("deletion failed is only logged");
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, true);
+
+		elasticsearchExceptionLogger.logOrThrowOnDeleteDocumentFailure(
+			new SearchException("deletion failed is only logged"));
+	}
+
+	@Test
+	public void testLogExceptionsOnlyFalse() throws Throwable {
+		expectedException.expect(SearchException.class);
+		expectedException.expectMessage("some other random message");
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, false);
+
+		elasticsearchExceptionLogger.logOrThrow(
+			new SearchException("some other random message"));
+	}
+
+	@Test
+	public void testLogExceptionsOnlyTrue() throws Throwable {
+		expectedLogTestRule.expectMessage("some random message");
+
+		ElasticsearchExceptionLogger elasticsearchExceptionLogger =
+			new ElasticsearchExceptionLogger(_log, true);
+
+		elasticsearchExceptionLogger.logOrThrowOnDeleteDocumentFailure(
+			new SearchException("some random message"));
+	}
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Rule
+	public ExpectedLogTestRule expectedLogTestRule = ExpectedLogTestRule.none();
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ElasticsearchExceptionLoggerTest.class);
+
+}
